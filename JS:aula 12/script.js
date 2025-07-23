6f695859aa2834611b771a0818532a8c7dfbfa48
Ruan Carlos Carvalho Gama <rcarvalhogama64@gmail.com>
2025-07-22 21:03:48 -0300

    correção da atividade

---
+++ JS:aula 12/script.js
@@ -0,0 +1,13 @@
+let nome= prompt("Qual o seu nome:")
+let numero= prompt("Digite um número:")
+let cidade= prompt("Qual a sua cidade?")
+
+console.log(nome)
+console.log(numero)
+console.log(cidade)
+
+numero = 1000
+console.log(numero)
+alert("Sua cidade é:" + cidade)
+
+
